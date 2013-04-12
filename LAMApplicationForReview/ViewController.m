56f22dae3392efa088d9a1dac9d6d8c6c98977f7
Anton Domashnev <antondomashnev@gmail.com>
2013-04-12 12:57:13 +0400

    Initial Commit

---
+++ LAMApplicationForReview/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LAMApplicationForReview
+//
+//  Created by Anton Domashnev on 12.04.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LAMApplicationForReview/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LAMApplicationForReview
+//
+//  Created by Anton Domashnev on 12.04.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
